f8b4c4edb295d3dcaabc520c2bb1143a04790880
GITHALIM <55746186+GITHALIM@users.noreply.github.com>
2020-03-10 16:43:20 +0900

    Create 11021.cpp

---
+++ 11021.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+using namespace std;
+int t, a, b;
+int main(){
+    scanf("%d", &t);
+    for(int i = 1; i < t + 1; i++){
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a+b);
+    }
+    return 0;
+}
